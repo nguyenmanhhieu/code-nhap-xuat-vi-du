f51f0da4aecdbfdf71bb14efc7cd4f70be79685b
nguyenmanhhieu <hieunm14092001@gmail.com>
2022-06-10 22:29:25 +0700

    bai 29 code vidu
    nguyen manh hieu

---
+++ bai29.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  bai29
+//
+//  Created by Hiếu Hee on 10/06/2022.
+//
+
+#include <stdio.h>
+
+void main()
+{
+    int a = 10;
+    float b = 4.67892345;
+    char ch = 'A';
+    printf("Integer data = %d\n",a);
+    printf("Float Data = %f\n",b);
+    printf("Character = %c\n",ch);
+    printf("This prints the string\n");
+    printf("%s","This also prints a string\n");
+}
